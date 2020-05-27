5302ac13738cf8bbc5300b22fb18b8d9012f8abc
sampopulous <65770070+sampopulous@users.noreply.github.com>
2020-05-27 22:44:56 +0530

    driver app

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={			
+	'ApiUrl':"http://sagsabzi.com/dev/driver/api",		
+	'DialogDefaultTitle':"Sagsabzi DriverApp",
+	'mapboxToken' : '',
+	'APIHasKey':"osksnasknashishaasaUGHM",
+	'debug': false
+};
